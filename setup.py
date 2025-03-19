c42738b229a6f1f3350e929a9696762593561dd7
Rjashby1 <rjudsonashby@gmail.com>
2025-03-18 20:00:06 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+from setuptools import setup
 
+setup(
+    name='Demo',
+    version='0.1',
+    description='A simple demonstration package',
+    url='https://github.com/Rjashby1/DS5100_MOD9_demo_SP_2025/tree/main/demo',
+    author='R J Ashby',
+    author_email='gsr3qz@virginia.edu',
+    license='MIT',
+    packages=['demo']
+)
